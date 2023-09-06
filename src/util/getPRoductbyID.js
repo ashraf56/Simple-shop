485f88d36f8c0aa99fb3b2a4abfca5e4bbcc54f9
ashraf56 <ashrafulfahim07@gmail.com>
2023-09-06 15:18:26 +0600

    2nd commit

--- src/util/getPRoductbyID.js
+++ src/util/getPRoductbyID.js
@@ -1,19 +1,19 @@
 
 const getPRoductbyID = async (id) => {
-  let apiURl=process.env.API_url
+  // let apiURl=process.env.API_url
 
-  try {
-    let res= await fetch(`${apiURl}/api/products/${id}`,{
-        cache:"no-cache"
-    })
-    if (!res.ok) {
-        throw  new Error("data load failed")
-    }
-    return res.json()
-  } catch (error) {
+  // try {
+  //   let res= await fetch(`${apiURl}/api/products/${id}`,{
+  //       cache:"no-cache"
+  //   })
+  //   if (!res.ok) {
+  //       throw  new Error("data load failed")
+  //   }
+  //   return res.json()
+  // } catch (error) {
   
-    console.log(error);
-  }
+  //   console.log(error);
+  // }
 };
 
 export default getPRoductbyID;
