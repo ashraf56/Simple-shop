530b40aae87182f5cc16d128991c36f638b656f7
ashraf56 <ashrafulfahim07@gmail.com>
2023-09-22 20:59:40 +0600

    api set

--- src/app/api/admins/route.js
+++ src/app/api/admins/route.js
@@ -1,7 +1,7 @@
 import { ConnectMongo } from "@/lib/db"
 import { Alluser } from "@/lib/modal/alluser"
 import { NextResponse } from "next/server"
-
+export const revalidate = 0;
 export async function GET() {
     await ConnectMongo()
     const user= await Alluser.find({role:'admin'})
