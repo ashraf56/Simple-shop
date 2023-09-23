cc4562fbef85dc4c2e926abd92a298023bcf99ac
ashraf56 <ashrafulfahim07@gmail.com>
2023-09-23 16:16:43 +0600

    random2

--- src/app/(home)/admins/admindetails/[id]/page.js
+++ src/app/(home)/admins/admindetails/[id]/page.js
@@ -1,9 +1,29 @@
 import getUserbyiD from '@/util/getUserbyiD';
 import React from 'react';
 
+const getOneUserbyiD = async(id) => {
+  
+   
+    let apiURl=process.env.API_url
+  
+    try {
+      let res= await fetch(`${apiURl}/api/user/${id}`,{
+          cache:"no-cache"
+      })
+      if (!res.ok) {
+          throw  new Error("data load failed")
+      }
+      return res.json()
+    } catch (error) {
+    
+      console.log(error);
+    }
+
+};
+
 const page = async ({params}) => {
 let {id}=params
-let {user} = await getUserbyiD(id)
+let {user} = await getOneUserbyiD(id)
     return (
         <div>
             {
